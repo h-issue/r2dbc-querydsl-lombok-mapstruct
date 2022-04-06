a73ffe3a881982111d8ad690c7dbbce4f4f92107
huahouye <huahouye@gmail.com>
2022-04-06 15:16:39 +0800

    [BUG] infobip-spring-data-r2dbc-querydsl-boot-starter can not work with @ org.springframework.data.annotation.Transient, run mvn clean -Dmaven.test.skip=true package && java -jar r2dbc-querydsl-lombok-mapstruct-server/target/r2dbc-querydsl-lombok-mapstruct-server-0.0.1-SNAPSHOT.jar can see exception.

--- r2dbc-querydsl-lombok-mapstruct-server/src/main/java/com/example/demo/domain/Order.java
+++ r2dbc-querydsl-lombok-mapstruct-server/src/main/java/com/example/demo/domain/Order.java
@@ -3,6 +3,7 @@ package com.example.demo.domain;
 import java.util.UUID;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Transient;
 import org.springframework.data.domain.Persistable;
 import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
@@ -26,8 +27,16 @@ public class Order extends BaseDomain implements Persistable<UUID> {
 	@Column("created_by")
 	private String createdBy;
 
+	@Transient
+	private boolean newEntity;
+
 	@Override
 	public boolean isNew() {
-		return getCreatedAt() == null;
+		return this.newEntity || getCreatedAt() == null;
+	}
+
+	public Order setAsNew() {
+		this.newEntity = true;
+		return this;
 	}
 }
--- r2dbc-querydsl-lombok-mapstruct-server/src/main/java/com/example/demo/domain/User.java
+++ r2dbc-querydsl-lombok-mapstruct-server/src/main/java/com/example/demo/domain/User.java
@@ -3,6 +3,7 @@ package com.example.demo.domain;
 import java.time.LocalDateTime;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Transient;
 import org.springframework.data.domain.Persistable;
 import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
@@ -35,8 +36,21 @@ public class User implements Persistable<Uid> {
 	@Column("updated_at")
 	private LocalDateTime updatedAt;
 
+//	@Override
+//	public boolean isNew() {
+//		return createdAt == null;
+//	}
+
+	@Transient
+	private boolean newEntity;
+
 	@Override
 	public boolean isNew() {
-		return createdAt == null;
+		return this.newEntity || createdAt == null;
+	}
+
+	public User setAsNew() {
+		this.newEntity = true;
+		return this;
 	}
 }
